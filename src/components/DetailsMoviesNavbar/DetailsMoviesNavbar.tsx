32f80eb46553cad3054a4bd15181c1cbea430ef4
Anton Pryadkin <zorg200687@gmail.com>
2020-07-01 23:01:04 +0300

    add links in DetailsMoviesNavbar component

--- src/components/DetailsMoviesNavbar/DetailsMoviesNavbar.tsx
+++ src/components/DetailsMoviesNavbar/DetailsMoviesNavbar.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 
 // styles
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import styles from "./DetailsMoviesNavbar.module.scss";
 
+// types
+import { IParams } from '../../redux/movieStateReducer/movieStateReducerTypes';
+
 const DetailsMoviesNavbar = () => {
+  const history = useHistory();
+  const { movie, page } = useParams<IParams>();
+  console.log(useParams<IParams>())
+
+  const onClickHandler = () => {
+    if (movie) {
+      history.push(`/search/${movie}/${page}`)
+    } else {
+      history.push(`/search`)
+    }
+  };
+
   return (
     <Navbar
       bg="dark"
@@ -18,17 +33,13 @@ const DetailsMoviesNavbar = () => {
         </Navbar.Brand>
 
         <Nav className="mr-auto">
-          <Route path="/profile">
-            <Nav.Link>
-              Home
-            </Nav.Link>
-          </Route>
+          <Nav.Link onClick={() => history.push(`/profile`)}>
+            Home
+          </Nav.Link>
 
-          <Route path="/search">
-            <Nav.Link>
-              Search
-            </Nav.Link>
-          </Route>
+          <Nav.Link onClick={onClickHandler}>
+            Search
+          </Nav.Link>
         </Nav>
       </Container>
     </Navbar>
